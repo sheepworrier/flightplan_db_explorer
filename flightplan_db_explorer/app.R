a4d81a18248aa5634e696b3882ada85a5cc1ad23
sheepworrier <djp42@cantab.net>
2020-02-22 21:50:06 +0000

    Remove sleep window constraint Add labels for each table Ensure inbound flights are after the 1st outbound

--- flightplan_db_explorer/app.R
+++ flightplan_db_explorer/app.R
@@ -23,26 +23,26 @@ ui <- fluidPage(
     fluidRow(
         column(width = 6,
                textInput("from", "Starting point:", value = "LGW"),
-               textInput("to", "Destination:", value = "AGP"),
-               textInput("earliest_departure", "Earliest departure time:",
-                         value = "10:00")),
+               textInput("to", "Destination:", value = "AGP")),
         column(width = 6,
-               textInput("sleep_start", "Start of sleep:", value = "12:00"),
-               textInput("sleep_end", "End of sleep:", value = "14:00"),
-               textInput("latest_arrival", "Latest arrival time:",
-                         value = "18:00"))),
+               textInput("earliest_departure", "Earliest departure time:",
+                         value = "10:00"),
+        textInput("latest_arrival", "Latest arrival time:",
+                         value = "19:00"))),
     column(width = 6,
+           h2("Outbound"),
            dataTableOutput("outbound_flights")),
     column(width = 6,
+           h2("Inbound"),
            dataTableOutput("inbound_flights"))
 
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$outbound_flights <- renderDataTable({
-        filtered <- segments %>%
+    
+    filtered_outbound <- reactive({
+        segments %>%
             filter(stops == 0,
                    fromCity == input$from,
                    toCity == input$to,
@@ -51,45 +51,42 @@ server <- function(input, output) {
                                                  input$earliest_departure)),
                    arrival <= as.POSIXct(paste(dummy_date, 
                                                input$latest_arrival)),
-                   (departure <= 
-                        as.POSIXct(paste(dummy_date, input$sleep_start)) &
-                        arrival <=
-                            as.POSIXct(paste(dummy_date, input$sleep_end))) |
-                       (departure >=
-                            as.POSIXct(paste(dummy_date, input$sleep_start)) &
-                            arrival >=
-                                as.POSIXct(paste(dummy_date,
-                                                 input$sleep_end)))) %>%
+                   departure <= as.POSIXct(paste(dummy_date, 
+                                               input$latest_arrival))) %>% 
             mutate(departure = format(departure, "%H:%M"),
                    arrival = format(arrival, "%H:%M")) %>%
-            select(date, departure, arrival)
-        DT::datatable(filtered,
-                      rownames = FALSE)
+            distinct(date, departure, arrival)
     })
 
-    output$inbound_flights <- renderDataTable({
-        filtered <- segments %>%
+    filtered_inbound <- reactive({
+        earliest_outbound <- filtered_outbound() %>%
+            arrange(date) %>%
+            head(1) %>%
+            pull(date)
+        segments %>%
             filter(stops == 0,
                    fromCity == input$to,
                    toCity == input$from,
                    lagDays == 0,
+                   date > !!earliest_outbound,
                    departure >= as.POSIXct(paste(dummy_date,
                                                  input$earliest_departure)),
                    arrival <= as.POSIXct(paste(dummy_date, 
                                                input$latest_arrival)),
-                   (departure <= 
-                        as.POSIXct(paste(dummy_date, input$sleep_start)) &
-                        arrival <=
-                        as.POSIXct(paste(dummy_date, input$sleep_end))) |
-                       (departure >=
-                            as.POSIXct(paste(dummy_date, input$sleep_start)) &
-                            arrival >=
-                            as.POSIXct(paste(dummy_date,
-                                             input$sleep_end)))) %>%
+                   departure <= as.POSIXct(paste(dummy_date, 
+                                                 input$latest_arrival))) %>%
             mutate(departure = format(departure, "%H:%M"),
                    arrival = format(arrival, "%H:%M")) %>%
-            select(date, departure, arrival)
-        DT::datatable(filtered,
+            distinct(date, departure, arrival)
+    })
+    
+    output$outbound_flights <- renderDataTable({
+        DT::datatable(filtered_outbound(),
+                      rownames = FALSE)
+    })
+
+    output$inbound_flights <- renderDataTable({
+        DT::datatable(filtered_inbound(),
                       rownames = FALSE)
     })
 
